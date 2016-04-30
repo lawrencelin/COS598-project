9d6dbe5ded432d334001e73fc70eba34b8ef3466
William Yang <wkyjyy@gmail.com>
2016-04-30 02:45:53 -0400

    1st version

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,17 +1,20 @@
 #include <NeuralNetwork.hpp>
 
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <numeric>
-#include <future>
+// #include <vector>
+// #include <algorithm>
+// #include <numeric>
+// #include <future>
 #include <thread>
+
+#include <cassert>
 
 using namespace std;
 using namespace NN;
 
-int main()
+int main(int argc, const char* argv[])
 {
+	assert("Must provide file name" && argc > 1);
 	unsigned int n = thread::hardware_concurrency();
 	cout << n << " concurrent threads are supported.\n";
 
@@ -19,11 +22,10 @@ int main()
 	typedef DataSet<float> float_dataset;
 	typedef BatchTrainer<float_network, float_dataset> float_trainer;
 
-	float_network ann({8, 16, 8, 1});
-
-	float_dataset ds("xor8.txt", 256, 8, 1);
-	float_trainer t(ann, ds);
-	float err = t.train(1);
+	float_network ann({8, 16, 16, 1});
+	float_dataset ds(argv[1], 256, 8, 1);
+	float_trainer t(ann, ds, 0.05, 0.9);
+	float err = t.train(10000);
 	cout<<"Final training RMSE: "<<err<<"\n";
 	return 0;
 }
